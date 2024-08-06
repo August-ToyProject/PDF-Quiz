3c2f99e255414dcf30a5ee09f03e8fcea953d894
KMK_HM <kyun9151@gmail.com>
2024-08-06 23:37:27 +0900

    feat: Swagger 설정 완료

--- BE/spring/quiz-application/src/main/java/com/quizapplication/controller/MemberController.java
+++ BE/spring/quiz-application/src/main/java/com/quizapplication/controller/MemberController.java
@@ -3,18 +3,23 @@ package com.quizapplication.controller;
 import com.quizapplication.dto.request.SignupDto;
 import com.quizapplication.dto.response.MemberResponse;
 import com.quizapplication.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "유저 관련 API 명세")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1")
 public class MemberController {
 
     private final MemberService memberService;
+
+    @Operation(summary = "회원가입", description = "회원가입시 사용하는 API")
 
     @PostMapping("/sign-up")
     public MemberResponse signup(@RequestBody SignupDto signupDto) {
