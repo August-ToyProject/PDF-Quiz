e1a6225b37c19eedbba0b8f08059ec1ce0ee697f
deokbin <deokbin@naver.com>
2024-08-07 14:11:47 +0900

    feat[#19]: 로그인 및 회원가입 페이지 라우팅 설정

--- FE/pdf_quiz/src/App.tsx
+++ FE/pdf_quiz/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignUp from "./pages/SignUp.tsx"
+import Login from './pages/Login.tsx';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SignUp/>} />
+        <Route path ="/" element={<Login/>}/>
+        <Route path="/signup" element={<SignUp/>} />
       </Routes>
     </BrowserRouter>
   );
