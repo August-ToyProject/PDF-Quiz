1268b294719815aed9854bfd81c01ec10862f801
KMK_HM <kyun9151@gmail.com>
2024-08-08 15:39:08 +0900

    feat: CORS 처리

--- BE/spring/quiz-application/src/main/java/com/quizapplication/config/security/SecurityConfig.java
+++ BE/spring/quiz-application/src/main/java/com/quizapplication/config/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.quizapplication.config.security;
 
+import java.util.Arrays;
+import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,6 +9,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -19,12 +24,27 @@ public class SecurityConfig {
                 .formLogin((formLogin) -> formLogin.disable())
                 .httpBasic((httpBasic) -> httpBasic.disable())
                 .headers((e) -> e.frameOptions((a) -> a.sameOrigin()))
-                .authorizeRequests((requests) -> requests.anyRequest().permitAll());
+                .authorizeRequests((requests) -> requests.anyRequest().permitAll())
+                .cors(cors -> cors.configurationSource(getCorsConfiguration()));
         return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    CorsConfigurationSource getCorsConfiguration() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOriginPatterns(Arrays.asList("*"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowCredentials(true);
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
---
+++ BE/spring/quiz-application/src/main/java/com/quizapplication/dto/request/LoginRequestDto.java
@@ -0,0 +1,9 @@
+package com.quizapplication.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class LoginRequestDto {
+    private String email;
+    private String password;
+}
