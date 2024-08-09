41d6fa5c68c10f4f0b9291b094eace4906593f00
KMK_HM <kyun9151@gmail.com>
2024-08-09 21:48:41 +0900

    feat: accessToken 발급 기능 개발

--- BE/spring/quiz-application/src/main/java/com/quizapplication/QuizApplication.java
+++ BE/spring/quiz-application/src/main/java/com/quizapplication/QuizApplication.java
@@ -8,7 +8,6 @@ public class QuizApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuizApplication.class, args);
-
 	}
 
 }
---
+++ BE/spring/quiz-application/src/main/java/com/quizapplication/config/jwt/TokenDto.java
@@ -0,0 +1,16 @@
+package com.quizapplication.config.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TokenDto {
+    private String grantType;
+    private String accessToken;
+    private long accessTokenExpiresIn;
+}
