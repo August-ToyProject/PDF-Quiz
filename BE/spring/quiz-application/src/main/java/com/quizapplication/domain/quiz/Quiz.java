e6fc684d8294d66636ab53b36b87f5444a6b538a
KMK_HM <kyun9151@gmail.com>
2024-08-20 18:36:17 +0900

    feat: 문제 db에 저장

--- BE/spring/quiz-application/src/main/java/com/quizapplication/domain/quiz/Quiz.java
+++ BE/spring/quiz-application/src/main/java/com/quizapplication/domain/quiz/Quiz.java
@@ -1,21 +1,39 @@
 package com.quizapplication.domain.quiz;
 
+import io.hypersistence.utils.hibernate.type.json.JsonType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
 
 @Getter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Quiz {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String difficulty;
+
+    private String question;
+
+    @Type(JsonType.class)
     @Column(columnDefinition = "jsonb", nullable = false)
-    private String content;
+    private String options;
+
+    private String answer;
+
+    private String description;
+
 
 }
