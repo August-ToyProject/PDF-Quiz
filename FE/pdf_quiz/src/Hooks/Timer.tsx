5d4f433b8553a7aefd7b9d9408dbc151cff633d5
deokbin <deokbin@naver.com>
2024-09-03 10:06:01 +0900

    feat: 타이머 context api 추가

--- FE/pdf_quiz/src/Hooks/Timer.tsx
+++ FE/pdf_quiz/src/Hooks/Timer.tsx
@@ -1,38 +1,51 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-const Timer= () => {
-    const [hours, setHours] = useState<number>(0);
-    const [minutes, setMinutes] = useState<number>(1);
-    const [seconds, setSeconds] = useState<number>(0);
+interface TimerProps {
+  hour: number;
+  minute: number;
+}
 
-    useEffect(() => {
-        const countdown = setInterval(() => {
-            if (seconds > 0) {
-                setSeconds(seconds => seconds - 1);
-            } else if (minutes > 0) {
-                setMinutes(minutes => minutes - 1);
-                setSeconds(59);
-            } else if (hours > 0) {
-                setHours(hours => hours - 1);
-                setMinutes(59);
-                setSeconds(59);
-            } else {
-                clearInterval(countdown);
-                alert('Time is up!');
-            }
-        }, 1000);
+const Timer = ({ hour, minute }: TimerProps) => {
+  const [hours, setHours] = useState<number>(hour);
+  const [minutes, setMinutes] = useState<number>(minute);
+  const [seconds, setSeconds] = useState<number>(0);
 
-        // Cleanup function to clear the interval when the component unmounts
-        return () => clearInterval(countdown);
-    }, [seconds, minutes, hours]);
-    return (
-        <div>
-            <h6>{hours.toString().padStart(2, '0')}:{minutes.toString().padStart(2, '0')}:{seconds.toString().padStart(2, '0')}</h6>
-        </div>
-    )
-    
+  useEffect(() => {
+    // hour와 minute이 변경될 때마다 상태를 초기화합니다.
+    setHours(hour);
+    setMinutes(minute);
+    setSeconds(0);
+  }, [hour, minute]);
 
+  useEffect(() => {
+    const countdown = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds((seconds) => seconds - 1);
+      } else if (minutes > 0) {
+        setMinutes((minutes) => minutes - 1);
+        setSeconds(59);
+      } else if (hours > 0) {
+        setHours((hours) => hours - 1);
+        setMinutes(59);
+        setSeconds(59);
+      } else {
+        clearInterval(countdown);
+        alert("Time is up!");
+      }
+    }, 1000);
+
+    // Cleanup function to clear the interval when the component unmounts
+    return () => clearInterval(countdown);
+  }, [seconds, minutes, hours]);
+  return (
+    <div>
+      <h6>
+        {hours.toString().padStart(2, "0")}:
+        {minutes.toString().padStart(2, "0")}:
+        {seconds.toString().padStart(2, "0")}
+      </h6>
+    </div>
+  );
 };
-
 
 export default Timer;
