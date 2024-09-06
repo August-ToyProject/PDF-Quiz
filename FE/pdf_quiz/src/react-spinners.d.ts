226e9e53ff265c1d96f7027511c50f980cedec24
Junyong <79856225+junyong1111@users.noreply.github.com>
2024-09-06 18:19:53 +0900

    fix[#142] : ci/cd 타입 에러
    - 임시 수정 확인 후 수정이 필요하면 수정해야함

---
+++ FE/pdf_quiz/src/react-spinners.d.ts
@@ -0,0 +1,12 @@
+declare module 'react-spinners' {
+    import * as React from 'react';
+
+    interface LoaderProps {
+      loading?: boolean;  // 필수 속성에서 선택적 속성으로 변경
+      color?: string;
+      size?: number | string;
+      css?: string;
+    }
+
+    export class SyncLoader extends React.Component<LoaderProps> {}
+  }
