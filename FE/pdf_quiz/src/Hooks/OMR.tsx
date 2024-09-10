1e59bc19dc1f65b066e6236feeeca9cab7a528fb
deokbin <deokbin@naver.com>
2024-09-11 00:47:29 +0900

    fix: OMR 디자인 수정

--- FE/pdf_quiz/src/Hooks/OMR.tsx
+++ FE/pdf_quiz/src/Hooks/OMR.tsx
@@ -34,7 +34,7 @@ const OMR = ({ quizCount, optionCount, handleOptionClick }: OMRProps) => {
               key={optionIndex}
               onClick={() => handleOptionClick(problemIndex, optionIndex + 1)}
             >
-              <span className="text-m" style={{ fontSize: "18px" }}>
+              <span style={{ fontSize: "25px" }}>
                 {choiceSymbols[optionIndex] || `(${optionIndex + 1})`}
               </span>
             </button>
