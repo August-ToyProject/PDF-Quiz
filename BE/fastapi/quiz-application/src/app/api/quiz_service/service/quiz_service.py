89bcceca764c8dbd71f051561f3ddbca3ca6104c
Junyong <79856225+junyong1111@users.noreply.github.com>
2024-09-12 11:42:26 +0900

    Fix : 키워드 개수 추가
    2 -> 5개로 수정

--- BE/fastapi/quiz-application/src/app/api/quiz_service/service/quiz_service.py
+++ BE/fastapi/quiz-application/src/app/api/quiz_service/service/quiz_service.py
@@ -1,24 +1,31 @@
+import asyncio
+
 from fastapi import HTTPException
 
-
-import asyncio
-from src.app.api.quiz_service.helpers.quiz_service import create_prompt_template, get_keyword_from_summary, get_subject_from_summary_docs, load_vector, make_quiz, summarize_document
+from src.app.api.quiz_service.helpers.quiz_service import (
+    create_prompt_template,
+    get_keyword_from_summary,
+    get_subject_from_summary_docs,
+    load_vector,
+    make_quiz,
+    summarize_document,
+)
 from src.app.api.quiz_service.shema.quiz_request import QuizRequest
 
 
 async def generate_quiz(request: QuizRequest):
     try:
-        #-- vector 스토어 꺼내오기 
+        #-- vector 스토어 꺼내오기
         vector_store = await load_vector(request.index_path)
         retriever = vector_store.as_retriever()
         docs = vector_store.similarity_search("", k=min(request.num_questions * 5, 10))
-        
+
         #-- 문서 요약
         llm, summary = await summarize_document(docs, temperature=0.1)
-        
+
         #-- 주제 선정
         subject = await get_subject_from_summary_docs(llm, retriever)
-        
+
         #-- 중요 키워드 추출
         keywords = await get_keyword_from_summary(
             llm,
@@ -26,9 +33,9 @@ async def generate_quiz(request: QuizRequest):
             retriever=retriever,
             subject=subject,
             num_questions=request.num_questions,
-            num_keywords=2
+            num_keywords=5
         )
-        
+
         #-- 퀴즈 chain and make quiz
         quiz = await make_quiz(
             retriever=retriever,
