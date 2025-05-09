ed7f4671163734206a1b86941b1a8415995fa8bf
JunYongPark <jypark@hotseleer.co.kr>
2025-05-09 22:53:03 +0900

    ai dir 분리2

--- BE/fastapi/quiz-application/src/app/api/quiz_service/shema/quiz_request.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class QuizRequest(BaseModel):
-    user_idx:int
-    email:str
-    index_path: str
-    num_questions: int = 5
-    choice_count:int = 5
-    difficulty:int = 1
